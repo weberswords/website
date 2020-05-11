f4b7b4b0e0f36082c9fc8c35aeef1632a915bd8a
Webs <weberswords@gmail.com>
2020-05-10 20:20:11 -0700

    Move nav to app level

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Home from './Components/Home/home';
+import Nav from './Components/Nav/nav';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-          <Home/>
+    	<Nav />
+        <Home />
     </div>
   );
 }
